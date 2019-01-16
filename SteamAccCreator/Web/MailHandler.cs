0f7745d75f40e4af2c533c3d036745573a5d0937
Wachirawich Ruangpatimakorn <32639831+Holfz@users.noreply.github.com>
2019-01-16 12:50:08 +0700

    Hime Hime
    Suki Suki Daisuki

--- SteamAccCreator/Web/MailHandler.cs
+++ SteamAccCreator/Web/MailHandler.cs
@@ -11,10 +11,10 @@ namespace SteamAccCreator.Web
         private readonly RestClient _client = new RestClient();
         private readonly RestRequest _request = new RestRequest();
 
-        public static string Provider = "@xitroo.com";
+        public static string Provider = "@inboxkitten.com";
 
-        private static readonly Uri MailboxUri = new Uri("https://api.xitroo.com/v1/mails");
-        private static readonly Uri MailUri = new Uri("https://api.xitroo.com/v1/mail");
+        private static readonly Uri MailboxUri = new Uri("https://api.xforce.family/api/inboxkitten/GetLatestMails");
+        private static readonly Uri MailUri = new Uri("https://api.xforce.family/api/inboxkitten/GetMail");
         private static readonly Uri SteamUri = new Uri("https://store.steampowered.com/account/newaccountverification?");
 
         //private static readonly Regex FromRegex = new Regex(@".Steam.*");
@@ -26,16 +26,15 @@ namespace SteamAccCreator.Web
             _request.Method = Method.GET;
             dynamic jsonResponse;
 
-            _request.AddParameter("locale", "en");
-            _request.AddParameter("mailAddress", address);
-            _request.AddParameter("mailsPerPage", "25");
-            _request.AddParameter("minTimestamp", "0");
+            _request.AddParameter("author", "Holfz");
+            _request.AddParameter("recipient", address);
 
             do
             {
-                _request.AddOrUpdateParameter("maxTimestamp", DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+                System.Threading.Thread.Sleep(10000);
                 var response = _client.Execute(_request);
                 jsonResponse = JsonConvert.DeserializeObject(response.Content);
+                System.Console.WriteLine(jsonResponse);
             } while (jsonResponse.totalMails < 1);
 
             _request.Parameters.Clear();
@@ -43,10 +42,11 @@ namespace SteamAccCreator.Web
             {
                 foreach (var mail in jsonResponse.mails)
                 {
+                    System.Console.WriteLine(mail);
                     //if (FromRegex.IsMatch(mail.from.Value))
                     if (mail.from.Value.Contains("noreply@steampowered.com"))
                     {
-                        var mailText = ReadMail(mail._id.Value);
+                        var mailText = ReadMail(mail.id.Value);
                         var confirmUri = GetConfirmUri(mailText);
                         ConfirmSteamAccount(confirmUri);
                     }
@@ -58,11 +58,9 @@ namespace SteamAccCreator.Web
             }
         }
 
-        private Uri GetConfirmUri(string base64Text)
+        private Uri GetConfirmUri(string bodyplain)
         {
-            var data = Convert.FromBase64String(base64Text);
-            var decodedString = Encoding.UTF8.GetString(data);
-            var matches = ConfirmMailRegex.Matches(decodedString);
+            var matches = ConfirmMailRegex.Matches(bodyplain);
             var token1 = matches[0].Groups[1].Value;
             var token2 = matches[0].Groups[2].Value;
             var tokenUri = "stoken=" + token1 + "&creationid=" + token2;
@@ -74,21 +72,21 @@ namespace SteamAccCreator.Web
         {
             _client.BaseUrl = MailUri;
             _request.Method = Method.GET;
-            _request.AddParameter("locale", "en");
-            _request.AddParameter("id", mailId);
+            _request.AddParameter("author", "Holfz");
+            _request.AddParameter("mailKey", mailId);
             var response = _client.Execute(_request);
             _request.Parameters.Clear();
 
             dynamic jsonResponse = JsonConvert.DeserializeObject(response.Content);
 
-            return jsonResponse.bodyHtmlStrict;
+            return jsonResponse["body-plain"];
         }
 
         private void ConfirmSteamAccount(Uri uri)
         {
             _client.BaseUrl = uri;
             _request.Method = Method.GET;
-            _client.Execute(_request);
+            var echoresponse = _client.Execute(_request);
             _request.Parameters.Clear();
         }
     }
